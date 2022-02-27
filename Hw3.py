50420b6816dbfc7b4edae028ea72ce1372541fb7
VessyP <100419309+VessyP@users.noreply.github.com>
2022-02-27 18:44:17 +0200

    Add files via upload

---
+++ Hw3.py
@@ -0,0 +1,96 @@
+
+# Променливи, типове данни и оператори
+
+
+# first_name = "John"
+# age = 29
+# gender = 'm'
+# is_married = True
+#
+
+# print(first_name)
+
+
+# first_name = "Vesselina"
+# last_name = "Peshterlieva"
+# print(first_name)
+
+
+# first_name = "John"
+# age = 29
+# height = 1.80
+# is_married = True
+#
+# print(type(first_name))
+# print(type(age))
+# print(type(height))
+# print(type(is_married))
+
+
+# x = 3
+# y = 10
+# result = x*y
+# print(x, '+', y, '=', result)
+
+
+
+# h = 13.66
+# a = 245.54
+# S = (a*h)/2
+#
+# print(S, 'см.')
+
+
+# first_name = 'John'
+# last_name = 'Smith'
+# age = 30
+# gender ='m'
+# employee_number = 27560003
+
+
+# a = 5
+# b = 10
+# a = 10
+# b = 5
+# print(a,b)
+
+
+# Side_a = input("Моля въведете височината на успоредника: ")
+# Side_b = input("Моля въведете ширината на успоредника: ")
+#
+# P = (2*int(Side_a)) + (2*int(Side_b))
+# S = int(Side_a) * int(Side_b)
+#
+# print("Периметърът на успоредника е:", P,"см., а лицето му е:", S, "кв.см.")
+
+
+
+# import math
+#
+# a = input("Моля въведете стойност за а: ")
+# b = input("Моля въведете стойност за b: ")
+#
+# c = math.sqrt(int(a)**2 + int(b)**2)
+# print(c)
+
+
+
+# import math
+#
+# r = input("Моля въведете стойност за радиуса: ")
+# S = math.pi*(int(r)**2)
+#
+# print("Лицето на кръс с радиус", r, "см. е: ", S)
+
+
+# inches = input("Моля въведете стойност в инчове: ")
+# centimeters = float(inches) / 2.54
+#
+# print("В сантиметри ", inches, "инча е равно на: ", centimeters)
+
+
+
+# centimeters = input("Моля въведете стойност в антиметри: ")
+# inches = float(centimeters)*2.54
+#
+# print("В инчове ", centimeters, "сантиметра е равно на: ", inches)
